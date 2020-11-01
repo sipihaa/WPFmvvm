8c5572c05ae7d02168b9be4673d7e4a92da851be
vlad.milin.2005@gmail.com <vlad.milin.2005@gmail.com>
2020-11-01 15:28:50 +0300

    Удаляем хлам из разметки

--- WPFmvvm/obj/Debug/netcoreapp3.1/App.g.i.cs
+++ WPFmvvm/obj/Debug/netcoreapp3.1/App.g.i.cs
@@ -62,7 +62,7 @@ namespace WPFmvvm {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/WPFmvvm;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WPFmvvm;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
